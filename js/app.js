6d1294b0ad58e42bd83e675beb26def94483f8b0
Alexander Allen <aallen@blazinweb.com>
2015-02-11 14:28:20 -0600

    setup complete

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ function app(){
         //js
         {url: "./bower_components/jquery/dist/jquery.min.js"},
         {url: "./bower_components/lodash/lodash.min.js"}
-        // {url: "./bower_components/backbone/backbone.js"}
+        {url: "./bower_components/backbone/backbone.js"}
     ).then(function(){
         document.querySelector("html").style.opacity = 1;
         // start app?
